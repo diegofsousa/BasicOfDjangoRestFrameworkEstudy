13983cc3c3d5255d22d7f54d6509458230a3a8d2
diegofsousa <diegofernando5672@gmail.com>
2017-04-07 19:37:07 -0300

    tutorial done

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from portfolios.views import PortfolioListView, PortfolioView
+
+helper_patterns = [
+	url(r'^portfolios/$', PortfolioListView.as_view(), name='portfolios'),
+	url(r'^portfolios/(?P<pk>[0-9]+)$', PortfolioView.as_view(), name='get_portfolio'),
+]
+
+urlpatterns = helper_patterns
--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 from django.contrib import admin
-from models import DadosPessoais
+from .models import DadosPessoais
 
 admin.site.register(DadosPessoais)
